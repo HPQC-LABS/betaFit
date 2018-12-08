d5997668494ae3b443aa0274680b7ede3089c1b2
Nike Dattani <dattani.nike@gmail.com>
2018-12-08 18:33:43 -0500

    Strange commit. Got updated to end of 8 March 2013 in folder:
    nlogn:~/Backup_Scienide2_2017_06_05/diat/Li2/A-X/2013-A-X/programs/betaFIT/Code/20Dec12> ls -l -t
    total 1396
    -rwxr-xr-x 1 ndattani pnroy 314960 Mar  8  2013 tbetaf.x
    -rw-r--r-- 1 ndattani pnroy  57524 Mar  8  2013 AF3X3potRet.o
    -rw-r--r-- 1 ndattani pnroy  41216 Mar  8  2013 llsqf.o
    -rw-r--r-- 1 ndattani pnroy 424944 Mar  8  2013 betaFIT.o
    -rw-r--r-- 1 ndattani pnroy  97172 Mar  8  2013 nllssrr.o
    -rw-r--r-- 1 ndattani pnroy  75270 Feb 26  2013 betaFIT.f
    -rw-r--r-- 1 ndattani pnroy   1000 Jan 13  2013 Makefile
    -rw-r--r-- 1 ndattani pnroy   7311 Dec 27  2012 dampF.f
    -rwxr-xr-x 1 ndattani pnroy 245441 Dec 15  2012 betaf.x
    -rw-r--r-- 1 ndattani pnroy   8493 Feb  7  2012 AF3X3potRet.f
    -rw-r--r-- 1 ndattani pnroy  28617 Jun  6  2010 nllssrr.f
    -rw-r--r-- 1 ndattani pnroy   6781 Jul 14  2008 S_bob.f
    -rw-r--r-- 1 ndattani pnroy   4589 Jul 14  2008 PNS_func.f
    -rw-r--r-- 1 ndattani pnroy   8200 Jul 10  2008 S_func.f
    -rw-r--r-- 1 ndattani pnroy  48381 Feb 21  2008 phiFIT1_2.f
    -rw-r--r-- 1 ndattani pnroy  13709 Aug  2  2007 2Dphifit.f
    -rw-r--r-- 1 ndattani pnroy  10546 Aug  4  2006 llsqf.f
    nlogn:~/Backup_Scienide2_2017_06_05/diat/Li2/A-X/2013-A-X/programs/betaFIT/Code/20Dec12> cp -a AF3
    
    Seeing from the last commit messages, you'll see that lal this commit does is send some files back in time.

--- AF3X3.f
+++ AF3X3.f
@@ -1,157 +1,127 @@
-      SUBROUTINE AF3X3(RDIST,CmVAL,RE,De,RE3,RE6,C6adj,C9adj,ULR,
-     1 DEIGM1,DEIGM3,DEIGM5,DEIGR,DEIGDe,MXMLR)
-
-      REAL*8          H(3,3),DM1(3,3),DM3(3,3),DM5(3,3),DR(3,3),DDe(3,3)
-      REAL*8           Q(3,3),CmVAL(MXMLR)
-      REAL*8           DEIGM1(1,1),DEIGM3(1,1),DEIGM5(1,1),DEIGR(1,1),
-     1                    DEIGDe(1,1)
-      DOUBLE PRECISION W(3)
-      DOUBLE PRECISION EIGVEC(3,1)
-      REAL*8           RDIST,RE,De,RE3,RE6,C6adj,C9adj,DELTAE,ULR 
-      REAL*8           RESID(3,1)
-
-      REAL*8           M1,M2,M3,M5
-
+c=======================================================================
+      SUBROUTINE AF3X3potRet(RDIST,DELTAE,C3val,C6val,C8val,De,ULR,
+     1                              DEIGM1,DEIGM3,DEIGM5,DEIGR,DEIGDe)
+c=======================================================================
+      REAL*8  H(3,3),DM1(3,3),DM3(3,3),DM5(3,3),DR(3,3),
+     1              DDe(3,3),Q(3,3)
+      REAL*8  DEIGM1(1,1),DEIGM3(1,1),DEIGM5(1,1),DEIGR(1,1),
+     1        DEIGDe(1,1), EIGVEC(3,1), RESID(3,1), W(3) 
+      REAL*8  RDIST,RDIST2,RDIST3,DELTAE,C3val,C6val,C8val,De,ULR,
+     1   RET,RETSig,RETPi,Modulus,M1,M3,M5,Z
       INTEGER          I,J,L,K
-      INTEGER          ISTATE,MXMLR
-      
-      DOUBLE PRECISION MODULUS
-      REAL*8           Z
-
-c M2 = C6sigma, M3 = C6 adj
-
-      M1 = CmVAL(1)
-      M2 = CmVAL(3)
-      M3 = C6adj
-      M5 = CmVAL(5)
-      DELTAE = CmVAL(2)
-
-c WRITE(25,*) 'Variables = "r", "U(r)","U(r)-U(r)^2/(4De)" ' 
-c WRITE(25,*) 'zone T = "U(r)"'
- 
-          DM1(1,1)=-(1.D0/(3.D0*RDIST**3)+M1/(De*6.D0*RDIST**6))
-          DM1(1,2)=(SQRT(2.D0)/6.D0)*(M1/(De*RDIST**6)+2/RDIST**3)
-          DM1(1,3)=SQRT(6.D0)/(6.D0*RDIST**3)
-          DM1(2,1)=DM1(1,2)
-          DM1(2,2)=-2.D0/(3.D0*RDIST**3)-M1/(3.D0*De*RDIST**6)
-          DM1(2,3)=SQRT(3.D0)/(6.D0*RDIST**3)
-          DM1(3,1)=DM1(1,3)
-          DM1(3,2)=DM1(2,3)
-          DM1(3,3)=0.D0
-
-          DM3(1,1)=-1.d0/(3.d0*RDIST**6)
-          DM3(1,2)=-SQRT(2.d0)*DM3(1,1)
-          DM3(1,3)=0.D0
-          DM3(2,1)=DM3(1,2)
-          DM3(2,2)=2.d0*DM3(1,1)
-          DM3(2,3)=0.D0
-          DM3(3,1)=DM3(1,3)
-          DM3(3,2)=DM3(2,3)
-          DM3(3,3)=0.D0
-
-          DM5(1,1)=DM3(1,1)/(RDIST**2)
-          DM5(1,2)=DM3(1,2)/(RDIST**2)
-          DM5(1,3)=0.D0
-          DM5(2,1)=DM3(1,2)
-          DM5(2,2)=DM3(2,2)/(RDIST**2)
-          DM5(2,3)=0.D0
-          DM5(3,1)=DM5(1,3)
-          DM5(3,2)=DM5(2,3)
-          DM5(3,3)=0.D0
-
-
-       DR(1,1)=(3.D0*M1*RDIST**5+6*M2*RDIST**2+8*M5)/(3.D0*RDIST**9)
-       DR(1,2)=-SQRT(2.D0)*DR(1,1)
-       DR(1,3)=-SQRT(6.D0)*(M1/(2.D0*RDIST**4))
-       DR(2,1)=DR(1,2)
-       DR(2,2)=2.D0*DR(1,1)
-       DR(2,3)=-SQRT(3.D0)*M1/(2.D0*RDIST**4)
-       DR(3,1)=DR(3,1)
-       DR(3,2)=DR(2,3)
-       DR(3,3)=0.D0
-
-       DDe(1,1)=M1**2/(12.D0*RDIST**6*De**2)
-       DDe(1,2)=-SQRT(2.D0)*DDe(1,1)
-       DDe(1,3)=0.D0
-       DDe(2,1)=DDe(1,2)
-       DDe(2,2)=2.D0*DDe(1,1)
-       DDe(2,3)=0.d0
-       DDe(3,1)=DDe(1,3)
-       DDe(3,2)=DDe(2,3)
-       DDe(3,3)=0.D0
-
-            CALL ZHEEVJ3(H,Q,W,RDIST,M1,DELTAE)
-	     
-	    L=1
-	    DO J=2,3
-	       IF (W(J) .LT. W(L)) THEN
-		   L=J
-	       ENDIF
-            ENDDO	
-
-   20	    ULR=-W(L)
-
-	    DO I=1,3	    
-		EIGVEC(I,1) = Q(I,L)
-	    ENDDO	
-
-   30       DEIGM1 =- MATMUL(TRANSPOSE(EIGVEC),MATMUL(DM1,EIGVEC))           
-   40       DEIGR   =- MATMUL(TRANSPOSE(EIGVEC),MATMUL(DR,EIGVEC))
-   50       DEIGDe = - MATMUL(TRANSPOSE(EIGVEC),MATMUL(DDe,EIGVEC))
-
-c            WRITE(25,600) RDIST ,ULR 
-c  600 FORMAT(2D16.7)
-c
-	    WRITE(26,601) RDIST , DEIGM1, DEIGR ,DEIGDe
-  601 FORMAT(3D16.7)	
-
-         Modulus = SQRABS(Z) 
-
+      M1= C3val
+      M3= C6val
+      M5= C8val
+      RET= 9.36423830d-4*RDIST
+      RETSig= DCOS(RET) + (RET)*DSIN(RET)
+      RETPi= RETSig - RET**2 *DCOS(RET)
+      RDIST2= RDIST**2
+      RDIST3= RDIST*RDIST2
+*      WRITE(25,*) 'Variables = "r", "U(r)","U(r)-U(r)^2/(4De)" ' 
+*      WRITE(25,*) 'zone T = "U(r)"'
+c  Initialize interaction matrix to 0.d0
+      DO  I= 1,3
+          H(I,I)=0.0D0
+          ENDDO
+ccccc Prepare interation matrix  H 
+      H(1,1)= -(M1*RETSig+ M3/(RDIST3)+M5/(RDIST3*RDIST2))/(3.d0*RDIST3)
+      H(1,2)= -(DSQRT(2.D0))*H(1,1)
+      H(2,1)= H(1,2)
+      H(1,3)= M1*RETPi/(DSQRT(6.D0)*RDIST3)
+      H(3,1)= H(1,3)
+      H(2,2)= 2*H(1,1) + DELTAE
+      H(2,3)= H(1,3)/DSQRT(2.d0)
+      H(3,2)= H(2,3)
+      H(3,3)= DELTAE
+cccccc Prepare radial derivative of interaction matrix (? is it needed ?)
+      DR(1,1)= (3.d0*M1*RETSig + 6.d0*M3/RDIST3 
+     1                  + 8.D0*M5/(RDIST3*RDIST2))/(3.d0*RDIST3*RDIST)
+      DR(1,2)= -DSQRT(2.d0)*DR(1,1)
+      DR(2,1)= DR(1,2)
+      DR(2,2)= 2.d0*DR(1,1)
+      DR(1,3)= -3.d0*H(1,3)/RDIST
+      DR(3,1)= DR(1,3)
+      DR(2,3)= -3.d0*H(2,3)/RDIST
+      DR(3,2)= DR(2,3)
+      DR(3,3)= 0.d0 
+cccccc Partial derivative of interaction matric  H  w.r.t.  C3
+      DM1(1,1)= -(RETSig + M1/(2.d0*De*RDIST3))/(3.d0*RDIST3)
+      DM1(1,2)= -DSQRT(2.d0)*DM1(1,1)
+      DM1(2,1)= DM1(1,2)
+      DM1(2,2)= 2.d0*DM1(1,1)
+      DM1(1,3)= RETPi/(DSQRT(6.d0)*RDIST3)
+      DM1(3,1)= DM1(1,3)
+      DM1(2,3)= DM1(1,3)/DSQRT(2.d0)
+      DM1(3,2)= DM1(2,3)
+      DM1(3,3)= 0.d0
+cccccc Partial derivative of interaction matric  H  w.r.t.  C6
+      DM3(1,1)= -1.d0/(3.d0*RDIST3**2)
+      DM3(1,2)= -SQRT(2.d0)*DM3(1,1)
+      DM3(1,3)= 0.D0
+      DM3(2,1)= DM3(1,2)
+      DM3(2,2)= 2.d0*DM3(1,1)
+      DM3(2,3)= 0.D0
+      DM3(3,1)= DM3(1,3)
+      DM3(3,2)= DM3(2,3)
+      DM3(3,3)= 0.D0
+cccccc Partial derivative of interaction matric  H  w.r.t.  C8
+      DM5(1,1)= DM3(1,1)/(RDIST2)
+      DM5(1,2)= DM3(1,2)/(RDIST2)
+      DM5(1,3)= 0.D0
+      DM5(2,1)= DM3(1,2)
+      DM5(2,2)= DM3(2,2)/(RDIST2)
+      DM5(2,3)= 0.D0
+      DM5(3,1)= DM5(1,3)
+      DM5(3,2)= DM5(2,3)
+      DM5(3,3)= 0.D0
+cccccc Partial derivative of interaction matric  H  w.r.t.  De
+      DDe(1,1)= M1**2/(12.D0*(RDIST3*De)**2)
+      DDe(1,2)= -SQRT(2.D0)*DDe(1,1)
+      DDe(1,3)= 0.D0
+      DDe(2,1)= DDe(1,2)
+      DDe(2,2)= 2.D0*DDe(1,1)
+      DDe(2,3)= 0.d0
+      DDe(3,1)= DDe(1,3)
+      DDe(3,2)= DDe(2,3)
+      DDe(3,3)= 0.D0
+cccccc Call subroutine to prepare and invert interaction matrix  H
+      CALL ZHEEVJ3(H,Q,W)
+      L=1
+ccc Nor - identify the lowest eigenvalue of  H  and label it  L
+      DO J=2,3
+          IF (W(J) .LT. W(L)) THEN
+              L=J
+              ENDIF
+          ENDDO  
+      ULR= -W(L)
+      DO I=1,3      
+          EIGVEC(I,1) = Q(I,L)
+          ENDDO  
+   30 DEIGM1= -MATMUL(TRANSPOSE(EIGVEC),MATMUL(DM1,EIGVEC))
+   40 DEIGM3= -MATMUL(TRANSPOSE(EIGVEC),MATMUL(DM3,EIGVEC))
+   50 DEIGM5= -MATMUL(TRANSPOSE(EIGVEC),MATMUL(DM5,EIGVEC))           
+   60 DEIGR = -MATMUL(TRANSPOSE(EIGVEC),MATMUL(DR,EIGVEC))
+   70 DEIGDe= -MATMUL(TRANSPOSE(EIGVEC),MATMUL(DDe,EIGVEC))
+c     WRITE(25,600) RDIST ,ULR 
+c 600 FORMAT(2D16.7)
+c     WRITE(26,601) RDIST , DEIGM1, DEIGR ,DEIGDe
+c 601 FORMAT(4D16.7)  
+      Modulus = (Z)**2 
       RETURN
-
-      CONTAINS
-
-* ----------------------------------------------------------------------------
-      SUBROUTINE ZHEEVJ3(H, Q, W,RDIST,M1,DELTAE)
-* ----------------------------------------------------------------------------
-      REAL*8           RDIST,M1,DELTAE
-
-      REAL*8           H(3,3),Q(3,3)
-      DOUBLE PRECISION W(3)
-
-      INTEGER          N
-      PARAMETER        ( N = 3 )
-
-      DOUBLE PRECISION SD, SO
-      REAL*8           S, T
-      DOUBLE PRECISION C
-      DOUBLE PRECISION G, B, Z
-      DOUBLE PRECISION THRESH
-      INTEGER          I, X, Y, R
-
-      DOUBLE PRECISION FUNCTION SQRABS
-
-* initialize matrix to 0.d0
-
-        DO I=1,3
-                    H(I,I)=0.0D0
-                    ENDDO
-
-* initialize matrix elements
- 
-       H(1,1)=-(1.D0/3.D0)*(M1/(RDIST**3)+M3/(RDIST**6)+M5/(RDIST**8))
-       H(1,2)=-(SQRT(2.D0))*H(1,1)
-       H(2,1)=H(1,2)
-       H(1,3)=(SQRT(6.D0)/6.D0)*M1/(RDIST)**3
-       H(3,1)=H(1,3)
-       H(2,2)=2*H(1,1) + DELTAE
-       H(2,3)=(SQRT(3.D0)/6)*M1/RDIST**3
-       H(3,2)=H(2,3)
-       H(3,3)=DELTAE
-
-
-*     Initialize Q to the identitity matrix
-*     --- This loop can be omitted if only the eigenvalues are desired ---
+      end
+**    CONTAINS
+*=======================================================================
+      SUBROUTINE ZHEEVJ3(H,Q,W)
+*=======================================================================
+c** Subroutine to setup and invert the matrix  H  and return 
+c   eigenvalues W and eigenvector matric  Q
+      INTEGER   N, I, X, Y, R
+      PARAMETER (N=3)
+      REAL*8    H(3,3),Q(3,3), W(3)
+      REAL*8    SD,SO,S,T,C,G,B,Z,THRESH
+cc    DOUBLE PRECISION FUNCTION SQRABS
+* Initialize Q to the identitity matrix
+* --- This loop can be omitted if only the eigenvalues are desired ---
       DO  X = 1, N
           Q(X,X) = 1.0D0
           DO  Y = 1, X-1
@@ -159,114 +129,108 @@ c
               Q(Y, X) = 0.0D0
               ENDDO
           ENDDO
-
-*     Initialize W to diag(A)
+* Initialize W to diag(A)
       DO  X = 1, N
-          W(X) = DREAL(H(X, X))
+**        W(X) = DREAL(H(X, X))
+          W(X) = H(X, X)
           ENDDO
-
-*     Calculate SQR(tr(A))
-      SD = 0.0D0
-      DO 30 X = 1, N
-        SD = SD + ABS(W(X))
-   30 CONTINUE
+* Calculate SQR(tr(A))
+      SD= 0.0D0
+      DO  X = 1, N
+          SD= SD + ABS(W(X))
+          ENDDO
       SD = SD**2
-
-*     Main iteration loop
-      DO 40 I = 1, 50
-*       Test for convergence
-        SO = 0.0D0
-        DO 50 X = 1, N
-          DO 51 Y = X+1, N
-            SO = SO + ABS(DREAL(H(X, Y)))
-   51     CONTINUE
-   50   CONTINUE
-        IF (SO .EQ. 0.0D0) THEN
-          RETURN
-        END IF
-
-        IF (I .LT. 4) THEN
-          THRESH = 0.2D0 * SO / N**2
-        ELSE
-          THRESH = 0.0D0
-        END IF
-
-*       Do sweep
-        DO 60 X = 1, N
-          DO 61 Y = X+1, N
-            G = 100.0D0 * ( ABS(DREAL(H(X, Y))) )
-            IF ( I .GT. 4 .AND. ABS(W(X)) + G .EQ. ABS(W(X))
-     $                    .AND. ABS(W(Y)) + G .EQ. ABS(W(Y)) ) THEN
-              H(X, Y) = 0.0D0
-            ELSE IF (ABS(DREAL(H(X, Y)))
-     $                 .GT. THRESH) THEN
-*             Calculate Jacobi transformation
-              B = W(Y) - W(X)
-              IF ( ABS(B) + G .EQ. ABS(B) ) THEN
-                T = H(X, Y) / B
-              ELSE
-                IF (B .LE. 0.0D0) THEN
-                  T = -2.0D0 * H(X, Y)
-     $                    /(SQRT(B**2 + 4.0D0 * SQRABS(H(X, Y))) - B)
-                ELSE IF (B .EQ. 0.0D0) THEN
-                  T = H(X, Y) * (1.0D0 / ABS(H(X, Y)))
-                ELSE
-                  T = 2.0D0 * H(X, Y)
-     $                    /(SQRT(B**2 + 4.0D0 * SQRABS(H(X, Y))) + B)
-                END IF
-              END IF
-
-              C = 1.0D0 / SQRT( 1.0D0 + SQRABS(T) )
-              S = T * C
-              Z = DREAL(T * (H(X, Y)))
-
-*             Apply Jacobi transformation
-              H(X, Y) = 0.0D0
-              W(X)    = W(X) - Z
-              W(Y)    = W(Y) + Z
-              DO 70 R = 1, X-1
-                T       = H(R, X)
-                H(R, X) = C * T - (S) * H(R, Y)
-                H(R, Y) = S * T + C * H(R, Y)
-   70         CONTINUE
-              DO 80, R = X+1, Y-1
-                T       = H(X, R)
-                H(X, R) = C * T - S * (H(R, Y))
-                H(R, Y) = S * (T) + C * H(R, Y)
-   80         CONTINUE
-              DO 90, R = Y+1, N
-                T       = H(X, R)
-                H(X, R) = C * T - S * H(Y, R)
-                H(Y, R) = (S) * T + C * H(Y, R)
-   90         CONTINUE
-
-*             Update eigenvectors
-*             --- This loop can be omitted if only the eigenvalues are desired ---
-              DO 100, R = 1, N
-                T       = Q(R, X)
-                Q(R, X) = C * T - (S) * Q(R, Y)
-                Q(R, Y) = S * T + C * Q(R, Y)
-  100         CONTINUE
+* Main iteration loop
+      DO  I = 1, 50
+* Test for convergence
+          SO = 0.0D0
+          DO  X = 1, N
+              DO  Y = X+1, N
+**                SO = SO + ABS(DREAL(H(X, Y)))
+                  SO = SO + ABS(H(X, Y))
+                  ENDDO
+              ENDDO
+          IF(SO.EQ.0.0D0) RETURN
+          IF (I .LT. 4) THEN
+              THRESH = 0.2D0 * SO / N**2
+            ELSE
+              THRESH = 0.0D0
             END IF
-   61     CONTINUE
-   60   CONTINUE
-   40 CONTINUE
-
+* Do sweep
+          DO  X= 1, N
+              DO  Y= X+1, N
+**                G= 100.0D0*(ABS(DREAL(H(X, Y))) )
+                  G= 100.0D0*(ABS(H(X, Y))) 
+                  IF((I.GT.4).AND.((ABS(W(X))+G).EQ.ABS(W(X)))
+     $                         .AND.((ABS(W(Y))+G).EQ.ABS(W(Y)))) THEN
+                      H(X, Y)= 0.0D0
+**                  ELSEIF(ABS(DREAL(H(X, Y))).GT.THRESH) THEN
+                    ELSEIF(ABS(H(X, Y)).GT.THRESH) THEN
+* Calculate Jacobi transformation
+                      B= W(Y) - W(X)
+                      IF((ABS(B)+G).EQ.ABS(B)) THEN
+                          T= H(X, Y) / B
+                        ELSE
+                          IF(B .LE. 0.0D0) THEN
+                              T= -2.0D0 * H(X, Y)
+     $                       /(SQRT(B**2 + 4.0D0*(H(X, Y))**2) - B)
+                            ELSE IF (B .EQ. 0.0D0) THEN
+                              T= H(X, Y) * (1.0D0 / ABS(H(X, Y)))
+                            ELSE
+                              T= 2.0D0 * H(X, Y)
+     $                       /(SQRT(B**2 + 4.0D0*(H(X, Y))**2) + B)
+                            ENDIF
+                        ENDIF
+                      C= 1.0D0 / SQRT( 1.0D0 + (T)**2 )
+                      S= T * C
+                      Z= T * (H(X, Y))
+cc                    Z= DREAL(T * (H(X, Y)))
+* Apply Jacobi transformation
+                      H(X, Y) = 0.0D0
+                      W(X)    = W(X) - Z
+                      W(Y)    = W(Y) + Z
+                      DO  R = 1, X-1
+                          T       = H(R, X)
+                          H(R, X) = C * T - (S) * H(R, Y)
+                          H(R, Y) = S * T + C * H(R, Y)
+                          ENDDO
+                      DO  R = X+1, Y-1
+                          T       = H(X, R)
+                          H(X, R) = C * T - S * (H(R, Y))
+                          H(R, Y) = S * (T) + C * H(R, Y)
+                          ENDDO
+                      DO  R = Y+1, N
+                          T       = H(X, R)
+                          H(X, R) = C * T - S * H(Y, R)
+                          H(Y, R) = (S) * T + C * H(Y, R)
+                          ENDDO
+* eigenvectors
+* This loop can be omitted if only the eigenvalues are desired ---
+                      DO  R = 1, N
+                          T       = Q(R, X)
+                          Q(R, X) = C * T - (S) * Q(R, Y)
+                          Q(R, Y) = S * T + C * Q(R, Y)
+                          ENDDO
+                      ENDIF
+                  ENDDO
+              ENDDO
+          ENDDO
       PRINT *, "ZHEEVJ3: No convergence."
-
-      END SUBROUTINE ZHEEVJ3
-
-* ----------------------------------------------------------------------------
-      DOUBLE PRECISION FUNCTION SQRABS(Z)
-* ----------------------------------------------------------------------------
-* Calculates the squared absolute value of a complex number Z
-* ----------------------------------------------------------------------------
-*     .. Parameters ..
-      REAL*8 Z
-
-      SQRABS = DREAL(Z)**2
+**    END SUBROUTINE ZHEEVJ3
       RETURN
+      END
 
-      END FUNCTION SQRABS
-
-      END SUBROUTINE AF3X3 
+*=======================================================================
+      DOUBLE PRECISION FUNCTION SQRABS(Z)
+*=======================================================================
+* Calculates the squared absolute value of a complex number Z
+* ----------------------------------------------------------------------
+*  Parameters ..
+      REAL*8 Z
+**    SQRABS = DREAL(Z)**2
+      SQRABS = (Z)**2
+      RETURN
+      END
+**      END FUNCTION SQRABS
+*
+**     END SUBROUTINE AF3X3potRet
